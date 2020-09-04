646d7a171a28f89f86e8cd41c91df3a249003838
Sangsaka Wira <sangsakawira@Sangsakas-MacBook-Pro.local>
2020-09-05 05:53:30 +0700

    first commit

---
+++ karya-ibu/index.php
@@ -0,0 +1,17 @@
+<?php
+    $nama = "Sangsaka Wira";
+?>
+
+<html>
+    <head>
+    </head>
+
+    <body>
+        <h1><?php echo "Halo nama saya adalah $nama" ?></h1>
+        <?php
+        for ($x = 0; $x <= 10; $x++) {
+        echo "The number is: $x <br>";
+        }
+        ?>
+    </body>
+</html>
