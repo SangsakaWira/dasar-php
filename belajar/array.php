b2190a767bc2c4729a045f917f6f21be3adacd31
Sangsaka Wira <sangsakawira@Sangsakas-MacBook-Pro.local>
2020-09-06 15:56:00 +0700

    detail lokasi  done

---
+++ belajar/array.php
@@ -0,0 +1,7 @@
+<?php
+    $string1 = ["Sangsaka Wira","Devita Rachmat"];
+    echo var_dump($string1);
+    $string1[] = "Mira Faujiah";
+    echo var_dump($string1);
+
+?>
--- karya-ibu/index.php
+++ karya-ibu/index.php
@@ -1,33 +1,15 @@
 <?php
-    $nama = "Sangsaka Wira";
+    $name = $_GET["name"];
+    $email = $_GET["email"];
 ?>
-
 <html>
     <head>
     </head>
 
     <body>
-        <h1><?php echo "Halo nama saya adalah $nama" ?></h1>
-        <?php
-            for ($x = 0; $x <= 10; $x++) {
-            echo "The number is: $x <br>";
-            }
-            
-        ?>
-        <?php 
-            $mantap = "String";
-            echo var_dump($mantap);
-        
-        ?>
-        <?php
-            $array1 = ['Sangsaka Wira','Devita Rachmat','Mustika Rahayu','Mira Faujiah'];
-            echo $array1[0];
-        ?>
-        <?php for($x = 0; $x <= count($array1); $x++) { ?>
-            <h1><?php echo $array1[$x] ?></h1>
-            <h4><?php echo time(); ?></h4>
-            <h4><?php echo date("F j, Y, g:i a"); ?></h4>
-        <?php } ?>
-
+        <form>
+            <h1><?php echo $name; ?></h1>
+            <h1><?php echo $email; ?></h1>
+        </form>
     </body>
 </html>
